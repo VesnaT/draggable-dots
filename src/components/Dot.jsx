c267fd7b66ab28cde1db3fa251060e8906502353
Vesna Tanko <tankovesna@hotmail.com>
2023-12-01 11:26:00 +0100

    Draggable dot

--- src/components/Dot.jsx
+++ src/components/Dot.jsx
@@ -1,5 +1,14 @@
+import Draggable from "react-draggable";
+import { useRef } from "react";
+
 const Dot = (props) => {
-  return <div className="dot">{props.content}</div>;
+  const nodeRef = useRef(null);
+
+  return (
+    <Draggable nodeRef={nodeRef}>
+      <div ref={nodeRef} className="dot">{props.content}</div>
+    </Draggable>
+  );
 };
 
 export default Dot;
