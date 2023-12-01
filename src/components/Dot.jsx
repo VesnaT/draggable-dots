f4053395172e2ea52644c84348e120f823b365a2
Vesna Tanko <tankovesna@hotmail.com>
2023-12-01 11:35:08 +0100

    Set color

--- src/components/Dot.jsx
+++ src/components/Dot.jsx
@@ -1,12 +1,40 @@
 import Draggable from "react-draggable";
-import { useRef } from "react";
+import { useState, useRef } from "react";
 
 const Dot = (props) => {
   const nodeRef = useRef(null);
+  const [color, setColor] = useState(props.color);
+  const [tempColor, setTempColor] = useState(color);
+  const [isEditing, setIsEditing] = useState(false);
 
   return (
     <Draggable nodeRef={nodeRef}>
-      <div ref={nodeRef} className="dot">{props.content}</div>
+      <div
+        ref={nodeRef}
+        className="dot"
+        style={{ background: color }}
+        onDoubleClick={() => setIsEditing(true)}
+      >
+        {isEditing ? (
+          <div>
+            <textarea
+              className="text-input"
+              value={tempColor}
+              onChange={(e) => setTempColor(e.target.value)}
+              onKeyDown={(e) => {
+                if (e.key === "Enter") {
+                  setColor(tempColor);
+                  setIsEditing(false);
+                } else if (e.key === "Escape") {
+                  setIsEditing(false);
+                }
+              }}
+            />
+          </div>
+        ) : (
+          ""
+        )}
+      </div>
     </Draggable>
   );
 };
--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -3,15 +3,15 @@ import { useState } from "react";
 
 const Grid = () => {
   const [dots, setDots] = useState([
-    { content: "Foo", id: 0 },
-    { content: "Bar", id: 1 },
-    { content: "Baz", id: 2 },
+    { id: 0, content: "Foo", color: "#ff0000" },
+    { id: 1, content: "Bar", color: "#00ff00" },
+    { id: 2, content: "Baz", color: "#0000ff" },
   ]);
 
   return (
     <div className="grid">
       {dots.map((dot) => (
-        <Dot content={dot.content} key={dot.id} />
+        <Dot content={dot.content} key={dot.id} color={dot.color} />
       ))}
     </div>
   );
